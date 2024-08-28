ef6cb661fdae01727f31eaab675adf1aea3c0c02
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-28 13:30:29 +0200

    Finished extensions

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,13 +1,18 @@
 package com.booleanuk.api.counter;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("counter")
 public class CounterController {
     private int counter;
+    private Map<String, Integer> counters = new HashMap<>();
 
     public CounterController() {
         this.counter = 0;
@@ -32,5 +37,30 @@ public class CounterController {
         return "The counter is " + this.counter;
     }
 
+    @GetMapping("/custom/{name}")
+    public String MakeOrGetCounter(@PathVariable String name) {
+        int value = counters.getOrDefault(name, 0);
+        counters.putIfAbsent(name, value);
+        return name + " has value: " + value;
+    }
 
+    @GetMapping("/custom/{name}/increment")
+    public String CustomIncrement(@PathVariable String name) {
+        int currentValue = counters.getOrDefault(name, 0);
+        int newValue = currentValue + 1;
+        counters.put(name, newValue);
+        return name + " has value: " + newValue;
+    }
+
+    @GetMapping("/custom/{name}/decrement")
+    public String CustomDecrement(@PathVariable String name) {
+        int currentValue = counters.getOrDefault(name, 0);
+        int newValue = currentValue - 1;
+        counters.put(name, newValue);
+        return name + " has value: " + newValue;
+    }
+
+    public Map<String, Integer> getCounters() {
+        return counters;
+    }
 }
