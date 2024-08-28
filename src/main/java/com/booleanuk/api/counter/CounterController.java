a5fa8132f5a9cab7e8398c20cc3acff56fadaa30
uthmel <melvin.uthayaseelan@no.experis.com>
2024-08-28 11:51:33 +0200

    Fixed core. Forgot to commit before this.

--- src/main/java/com/booleanuk/api/counter/CounterController.java
+++ src/main/java/com/booleanuk/api/counter/CounterController.java
@@ -1,5 +1,36 @@
 package com.booleanuk.api.counter;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("counter")
 public class CounterController {
+    private int counter;
+
+    public CounterController() {
+        this.counter = 0;
+    }
+
+
+    @GetMapping()
+    public String getCounter() {
+        this.counter = 0;
+        return "The counter is " + this.counter;
+    }
+
+    @GetMapping("/increment")
+    public String increment() {
+        this.counter += 1;
+        return "The counter is " + this.counter;
+    }
+
+    @GetMapping("/decrement")
+    public String decrement() {
+        this.counter -= 1;
+        return "The counter is " + this.counter;
+    }
+
 
 }
